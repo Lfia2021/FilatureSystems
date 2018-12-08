d18179b6c53777ecb67dbb26e4c3c5bc41f921f9
JiweiHuang <huangjiwei@gxust.edu.cn>
2018-12-08 16:06:30 +0800

    2018-12-07 23:41 update

---
+++ src/main/scala/cn/edu/gxust/jiweihuang/scala/math/fitting/QuadraticVertexFitter.scala
@@ -0,0 +1,17 @@
+package cn.edu.gxust.jiweihuang.scala.math.fitting
+
+import java.util
+
+import cn.edu.gxust.jiweihuang.scala.math.function.univariate.TQuadraticVertex
+import org.hipparchus.fitting.{AbstractCurveFitter, WeightedObservedPoint}
+import org.hipparchus.optim.nonlinear.vector.leastsquares.LeastSquaresProblem
+
+class QuadraticVertexFitter(val initialGuess: Array[Double], val maxIter: Int) extends AbstractCurveFitter {
+  val FUNCTION: TQuadraticVertex.Parametric = new TQuadraticVertex.Parametric()
+
+  override def getProblem(points: util.Collection[WeightedObservedPoint]): LeastSquaresProblem = ???
+}
+
+object QuadraticVertexFitter{
+
+}

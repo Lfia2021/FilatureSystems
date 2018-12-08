1a57ccb0d19a26588b66b9ed1bb497ecf0171c06
JiweiHuang <huangjiwei@gxust.edu.cn>
2018-12-08 14:51:18 +0800

    2018-12-07 23:41 add math package

--- src/main/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/TQuadraticVertexLogistic.scala
+++ src/main/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/TQuadraticVertexLogistic.scala
@@ -20,6 +20,7 @@ trait TQuadraticVertexLogistic extends TUnivariateDifferentiableFunction
   override val formula: String = s"($quadraticVertexA*pow(x-$quadraticVertexB,2)+$quadraticVertexC)*$logisticM / (1 + exp(-$logisticK*(x-$logisticX0))) + $quadraticVertexLogisticD"
   val quadraticVertex: TQuadraticVertex = QuadraticVertex(quadraticVertexA, quadraticVertexB, quadraticVertexC)
   val logistic: TLogistic = Logistic(logisticM, logisticK, logisticX0)
+
   override def derivative(x: Double): Double = quadraticVertex.derivative(x) * logistic.value(x) + logistic.derivative(x) * quadraticVertex.value(x)
 
   /**
@@ -28,13 +29,16 @@ trait TQuadraticVertexLogistic extends TUnivariateDifferentiableFunction
     * @param x independent variable.
     * @return the integral value.
     */
-  override def integrate(x: Double): Double = quadraticVertexLogisticD * x + quadraticVertexA * logisticM * pow(x, 3) / 3 -
-    2 * quadraticVertexA * quadraticVertexB * logisticM * x * log(1 + exp(logisticK * (x - logisticX0))) / logisticK +
-    quadraticVertexA * pow(quadraticVertexB, 2) * logisticM * log(exp(logisticK * x) + exp(logisticK * logisticX0)) / logisticK +
-    quadraticVertexC * logisticM * log(exp(logisticK * x) + exp(logisticK * logisticX0)) / logisticK +
-    quadraticVertexA * logisticM * pow(x, 2) * log(1 + exp(logisticK * (logisticX0 - x))) / logisticK
+  override def integrate(x: Double): Double = {
+    quadraticVertexLogisticD * x + quadraticVertexA * logisticM * pow(x, 3) / 3 -
+      2 * quadraticVertexA * quadraticVertexB * logisticM * x * log(1 + exp(logisticK * (x - logisticX0))) / logisticK +
+      quadraticVertexA * pow(quadraticVertexB, 2) * logisticM * log(exp(logisticK * x) + exp(logisticK * logisticX0)) / logisticK +
+      quadraticVertexC * logisticM * log(exp(logisticK * x) + exp(logisticK * logisticX0)) / logisticK +
+      quadraticVertexA * logisticM * pow(x, 2) * log(1 + exp(logisticK * (logisticX0 - x))) / logisticK
+  }
 
   override def value(ds: DerivativeStructure): DerivativeStructure = quadraticVertex.value(ds).multiply(logistic.value(ds)).add(quadraticVertexLogisticD)
+
   override def value(x: Double): Double = logistic.value(x) * quadraticVertex.value(x) + quadraticVertexLogisticD
 
 }
---
+++ src/test/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/QuadraticVertexLogisticTest.scala
@@ -0,0 +1,25 @@
+package cn.edu.gxust.jiweihuang.scala.math.function.univariate
+
+import cn.edu.gxust.jiweihuang.scala.test.UnitSpec
+
+class QuadraticVertexLogisticTest extends UnitSpec {
+  "" should "" in {
+    println(QuadraticVertexLogistic(1e-2,1200,0,1,0.005,300,1.5).integrate(0,1000))
+    println(QuadraticVertexLogistic(1e-2,1200,0,1,0.005,300,1.5).integrateRomberg(0, 1000))
+    println(QuadraticVertexLogistic(1e-2,1200,0,1,0.005,300,1.5).integrateSimpson(0, 1000))
+    println(QuadraticVertexLogistic(1e-2,1200,0,1,0.005,300,1.5).integrateTrapezoid(0, 1000))
+    println(QuadraticVertexLogistic(1e-2,1200,0,1,0.005,300,1.5).integrateMidPoint(0, 1000))
+    println(QuadraticVertexLogistic(1e-2,1200,0,1,0.005,300,1.5).integrateIterativeLegendreGauss(0, 1000))
+  }
+
+  "f" should "g" in {
+    println(QuadraticVertexLogistic(3).differential(1))
+    println(QuadraticVertexLogistic(3).derivative(1))
+    println(QuadraticVertexLogistic(4, 5).differential(2))
+    println(QuadraticVertexLogistic(4, 5).derivative(2))
+    println(QuadraticVertexLogistic(4, 5, 6, 7).differential(3))
+    println(QuadraticVertexLogistic(4, 5, 6, 7).derivative(3))
+    println(QuadraticVertexLogistic(8, 9, 10, 11, 0, 2, 3).differential(4))
+    println(QuadraticVertexLogistic(8, 9, 10, 11, 0, 2, 3).derivative(4))
+  }
+}
